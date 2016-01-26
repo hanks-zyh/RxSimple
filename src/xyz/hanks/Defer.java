b1f4d3822188e67385b0cac361d84e05c4dd80da
hanks-zyh <zhangyuhan2014@gmail.com>
2016-01-26 16:59:28 +0800

    defer operator

---
+++ src/xyz/hanks/Defer.java
@@ -0,0 +1,45 @@
+package xyz.hanks;
+
+import rx.Observable;
+import rx.functions.Action1;
+import rx.functions.Func0;
+import rx.functions.Func1;
+
+/**
+ * Created by hanks on 16/1/26.
+ */
+public class Defer {
+    public static void main(String[] args) {
+        User user = new User();
+        user.setName("hanks");
+        user.getName().map(new Func1<String, String>() {
+            @Override
+            public String call(String s) {
+                return "UserName:"+s;
+            }
+        }).subscribe(new Action1<String>() {
+            @Override
+            public void call(String s) {
+                System.out.println(s);
+            }
+        });
+    }
+}
+
+
+class User{
+    private  String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Observable<String> getName(){
+        return Observable.defer(new Func0<Observable<String>>() {
+            @Override
+            public Observable<String> call() {
+                return Observable.just(name);
+            }
+        });
+    }
+}
