41bc1e4f3ffd797672db7cc2ab0c39d8968ff7d1
hanks-zyh <zhangyuhan2014@gmail.com>
2016-01-27 22:11:18 +0800

    from

---
+++ src/xyz/hanks/From.java
@@ -0,0 +1,19 @@
+package xyz.hanks;
+
+import rx.Observable;
+import rx.functions.Action1;
+
+/**
+ * Created by hanks on 16/1/27.
+ */
+public class From {
+    public static void main(String[] args) {
+        Integer[] nums = new Integer[]{1, 2, 3, 4, 5};
+        Observable.from(nums).subscribe(new Action1<Integer>() {
+            @Override
+            public void call(Integer integer) {
+                System.out.println("print:" + integer);
+            }
+        });
+    }
+}
