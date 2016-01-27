fe680e018b81d82282cc8eb843891f3f2df897f4
hanks-zyh <zhangyuhan2014@gmail.com>
2016-01-27 23:05:45 +0800

    timer

---
+++ src/xyz/hanks/Timer.java
@@ -0,0 +1,25 @@
+package xyz.hanks;
+
+import rx.Observable;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by hanks on 16/1/27.
+ */
+public class Timer {
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+        Observable
+                .timer(5, TimeUnit.SECONDS)
+                .subscribe(s -> System.out.println("trigger = " + (System.currentTimeMillis()-startTime)),
+                        throwable -> System.out.println(throwable.getMessage()),
+                        () -> System.out.println("onCompleted"));
+        try {
+            Thread.sleep(8*1000);
+            System.out.println("end");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
