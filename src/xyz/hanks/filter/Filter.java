d394eba4d8e2e043cf5fd640e73a6c1e6b94c96f
hanks-zyh <zhangyuhan2014@gmail.com>
2016-02-19 23:46:02 +0800

    learn filter

---
+++ src/xyz/hanks/filter/Filter.java
@@ -0,0 +1,20 @@
+package xyz.hanks.filter;
+
+import rx.Observable;
+import rx.functions.Func1;
+
+/**
+ * Created by hanks on 16/2/19.
+ */
+public class Filter {
+    public static void main(String[] args) {
+        Observable.just(1,2,3,4,5)
+                .filter(new Func1<Integer, Boolean>() {
+                    @Override
+                    public Boolean call(Integer integer) {
+                        return integer % 2 ==0; // return false will be abort
+                    }
+                })
+                .subscribe(s-> System.out.println("s = " + s));
+    }
+}
